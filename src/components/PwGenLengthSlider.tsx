5280b04ad48de4e0c47fa9d4a4830060b8085aab
janDo-dev <dev@jndnth.de>
2021-11-16 15:53:32 +0100

    refactor: add label for length slider

--- src/components/PwGenLengthSlider.tsx
+++ src/components/PwGenLengthSlider.tsx
@@ -5,17 +5,20 @@ const PwGenLengthSlider: React.FC = () => {
   const { length, updateLength } = useContext(PwGenContext);
 
   return (
-    <input
-      type="range"
-      name="pw-length"
-      id="pw-length-slider"
-      value={length.length}
-      min={length.lengthMin}
-      max={length.lengthMax}
-      onChange={(e) => {
-        updateLength(e.target.value);
-      }}
-    />
+    <>
+      <label htmlFor="pw-length">{length.length}</label>
+      <input
+        type="range"
+        name="pw-length"
+        id="pw-length-slider"
+        value={length.length}
+        min={length.lengthMin}
+        max={length.lengthMax}
+        onChange={(e) => {
+          updateLength(e.target.value);
+        }}
+      />
+    </>
   );
 };
 
